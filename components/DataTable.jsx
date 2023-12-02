ac1e497989dc90b6522b962fcd2cd0c122170449
Paula Cano <paulacanoroman@gmail.com>
2023-12-02 18:26:20 +0100

    feat: data consume with fetch and show data in console

--- components/DataTable.jsx
+++ components/DataTable.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const DataTable = () => {
+
+    const [dataAPI, setSataAPI] = useState([])
+
+    useEffect(() => {
+      const apiConsume = async ()=>{
+        const url = await fetch(`https://randomuser.me/api/?results=20`);
+        const data = await url.json();
+        console.log(data.results)
+      }
+      apiConsume()
+    }, [])
+    
   return (
     <div>dataTable</div>
   )
