5e1ae6e419ed0b8230c14fa15ce966722680d7c3
Paula Cano <paulacanoroman@gmail.com>
2023-12-02 19:09:09 +0100

    feat: split pop and join

--- components/DeleteLastWord.jsx
+++ components/DeleteLastWord.jsx
@@ -2,15 +2,23 @@ import React, { useState } from 'react'
 
 const DeleteLastWord = () => {
 
-    const [dataInput, setDataInput] = useState()
+    const [dataInput, setDataInput] = useState([]);
+
+    const handleDeleteLastWord = ()=>{
+        const sentence = dataInput.split("")
+        sentence.pop()
+        setDataInput(sentence.join(""))
+    }
 
 
   return (
     <div>
-        {dataInput}
+        
         <h3>Write the sentence</h3>
         <input onChange={(e)=>{setDataInput(e.target.value)}}></input>
-        <button onClick={()=>{}}>Delete last word</button>
+        <button onClick={()=>{handleDeleteLastWord()}}>Delete last word</button>
+        {dataInput}
+
     </div>
   )
 }
