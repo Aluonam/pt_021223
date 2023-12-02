84ff4dfd9d8180710b5b5aa141f368276a55b269
Paula Cano <paulacanoroman@gmail.com>
2023-12-02 18:27:31 +0100

    defensive programming with try catch

--- components/DataTable.jsx
+++ components/DataTable.jsx
@@ -5,12 +5,14 @@ const DataTable = () => {
     const [dataAPI, setSataAPI] = useState([])
 
     useEffect(() => {
-      const apiConsume = async ()=>{
-        const url = await fetch(`https://randomuser.me/api/?results=20`);
-        const data = await url.json();
-        console.log(data.results)
-      }
-      apiConsume()
+        try{     
+            const apiConsume = async ()=>{
+            const url = await fetch(`https://randomuser.me/api/?results=20`);
+            const data = await url.json();
+            console.log(data.results)
+        }
+        apiConsume()
+        }catch(error){error, "error detected"}
     }, [])
     
   return (
