208813ad9050ce5ca33c6251678397fb829a4718
Paula Cano <paulacanoroman@gmail.com>
2023-12-02 18:36:23 +0100

    feat: show data api

--- components/DataTable.jsx
+++ components/DataTable.jsx
@@ -16,7 +16,16 @@ const DataTable = () => {
         }catch(error){error, "error detected"}
     }, [])
     
-   
+    const tableData = dataAPI.map((element)=>{
+        return(
+            <tr>
+                <th>{element.id.value}</th>
+                <th>{element.location.city}</th>
+                <th>{element.name.first}{element.name.last}</th>
+                <th>Show more</th>
+            </tr>
+        )
+    })
 
   return (
     <>
@@ -31,12 +40,7 @@ const DataTable = () => {
             </tr>
         </thead>
         <tbody>
-        <tr>
-                <th>Id</th>
-                <th>Location</th>
-                <th>Name</th>
-                <th>Show more</th>
-            </tr>
+            {tableData}
         </tbody>
     </table>
     </>
