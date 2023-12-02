e7ccdef9c966f978cdbf2c8032c0d16a5e5c10a6
Paula Cano <paulacanoroman@gmail.com>
2023-12-02 18:53:34 +0100

    feat: turn de sentece with split, reverse and join

--- components/TurnSentence.jsx
+++ components/TurnSentence.jsx
@@ -2,13 +2,21 @@ import React, { useState } from 'react'
 
 const TurnSentence = () => {
 
-    const [dataInput, setDataInput] = useState("")
+    const [dataInput, setDataInput] = useState([])
+    const [newSentence, setNewSentence] = useState()
+
+    const turnTheSentence = ()=>{
+        const arrWords = dataInput.split("").reverse().join("");
+        setNewSentence(arrWords)
+    }
 
   return (
     <>
     <h3>Write the sentence</h3>
-    <input onChange={(e)=>{setDataInput(e)}}></input>
-    <button onClick={()=>{}}>Turn over</button>
+    <input onChange={(e)=>{setDataInput(e.target.value)}}></input>
+    <button onClick={()=>{turnTheSentence()}}>Turn over</button>
+    <br/>
+    {newSentence}
     </>
   )
 }
